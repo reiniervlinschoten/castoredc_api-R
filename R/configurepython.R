7134bf2aea1b68d87bedb3aac70738ad271fbd10
Reinier van Linschoten <reinier.v.linschoten@gmail.com>
2021-10-06 16:27:18 +0200

    added configuration function

--- R/configurepython.R
+++ R/configurepython.R
@@ -1,4 +1,19 @@
-.onLoad <- function(libname, pkgname) {
-  reticulate::conda_create("castoredc_api", python_version="3.8")
-  reticulate::configure_environment(pkgname)
+#' Sets up a conda environment called castoredc_api for exporting Castor EDC data
+#' If environment already exists, loads the environment and installs necessary packages.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' configure_python()
+#' }
+configure_python <- function(libname, pkgname) {
+  if (!("castoredc_api" %in% reticulate::conda_list()$name)) {
+    reticulate::conda_create(envname="castoredc_api",
+                             packages=c("castoredc_api"),
+                             python_version="3.8")
+  }
+  else {
+    reticulate::use_condaenv("castoredc_api")
+    reticulate::conda_install(envname="castoredc_api", "castoredc_api")
+  }
 }
